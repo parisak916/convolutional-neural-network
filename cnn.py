7a8471aace19c44d596b7d095202d33659001037
Parisa Khan <parisak916@gmail.com>
2019-11-11 16:44:42 -0500

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,298 @@
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+
+#Load the training and testing data
+#x_train and x_test parts contain greyscale RGB codes (from 0 to 255) while y_train and y_test parts contains labels from 0 to 9 which represents which number they actually are. 
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_test_orig = X_test
+# Select the subset from the train data for the sake of time.
+np.random.seed(1338)  # for reproducibilty!!
+# The subset is composed of all the examples where the digit is 7, and 20,000 examples are not 7.
+#sevens is indexes
+sevens = np.where(y_train == 7)[0].tolist()
+#notsevens is indexes
+not_sevens = np.where(y_train != 7)[0].tolist()
+num_keepers = 20000
+not_sevens = np.random.choice(not_sevens, num_keepers, replace=False).tolist()
+subset = sevens + not_sevens
+np.random.shuffle(subset) # shuffle the input
+
+X_train = X_train[subset, :, :]
+y_train = y_train[subset]
+
+from keras import backend as K
+
+img_rows, img_cols = 28, 28
+
+if K.image_data_format() == 'channels_first':
+    shape_ord = (1, img_rows, img_cols)
+else:  # channel_last
+    shape_ord = (img_rows, img_cols, 1)
+
+# Normalize the images:
+
+X_train = X_train.reshape((X_train.shape[0],) + shape_ord)
+X_test = X_test.reshape((X_test.shape[0],) + shape_ord)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+# Create labels:
+
+# Converting the labels to binary classification(Seven =1,Not Seven=0)
+
+Y_train = (y_train == 7).astype(int)
+Y_test = (y_test == 7).astype(int)
+
+# Converting the classes to its binary categorical form
+nb_classes = 2
+Y_train = np_utils.to_categorical(Y_train, nb_classes)
+Y_test = np_utils.to_categorical(Y_test, nb_classes)
+
+# -- Initializing the values for the convolution neural network
+
+nb_epoch = 2  # kept very low! Please increase if you have GPU
+
+batch_size = 64
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
+# Vanilla SGD
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
+model = Sequential()
+
+model.add(Conv2D(nb_filters, (nb_conv, nb_conv), padding='valid', 
+                 input_shape=shape_ord))  # note: the very first layer **must** always specify the input_shape
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(26, 26)))
+model.add(Flatten())
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+hist = model.fit(X_train, Y_train, batch_size=batch_size, 
+              epochs=10, verbose=1, 
+              validation_data=(X_test, Y_test))
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.legend(['Training', 'Validation'])
+
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.legend(['Training', 'Validation'], loc='lower right')
+
+loss, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+print('Test Loss:', loss)
+print('Test Accuracy:', accuracy)
+
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+slice = 15
+predicted = model.predict(X_test[:slice]).argmax(-1)
+
+plt.figure(figsize=(16,8))
+for i in range(slice):
+    plt.subplot(1, slice, i+1)
+    plt.imshow(X_test_orig[i], interpolation='nearest')
+    plt.text(0, 0, predicted[i], color='black', 
+             bbox=dict(facecolor='white', alpha=1))
+    plt.axis('off')
+
+
+model = Sequential()
+model.add(Conv2D(nb_filters, (nb_conv, nb_conv),
+                 padding='valid', input_shape=shape_ord))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train, batch_size=batch_size, 
+          epochs=nb_epoch,verbose=1,
+          validation_data=(X_test, Y_test))
+
+#Evaluating the model on the test data    
+score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score)
+print('Test accuracy:', accuracy)
+
+
+slice = 15
+predicted = model.predict(X_test[:slice]).argmax(-1)
+
+plt.figure(figsize=(16,8))
+for i in range(slice):
+    plt.subplot(1, slice, i+1)
+    plt.imshow(X_test_orig[i], interpolation='nearest')
+    plt.text(0, 0, predicted[i], color='black', 
+             bbox=dict(facecolor='white', alpha=1))
+    plt.axis('off')
+
+model = Sequential()
+model.add(Conv2D(nb_filters, (nb_conv, nb_conv),
+                 padding='valid', input_shape=shape_ord))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Conv2D(nb_filters, (nb_conv, nb_conv), 
+                 padding='valid'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Flatten())
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train, batch_size=batch_size, 
+          epochs=nb_epoch,verbose=1,
+          validation_data=(X_test, Y_test))
+
+#Evaluating the model on the test data    
+score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score)
+print('Test accuracy:', accuracy)
+
+predicted = model.predict(X_test[:slice]).argmax(-1)
+
+plt.figure(figsize=(16,8))
+for i in range(slice):
+    plt.subplot(1, slice, i+1)
+    plt.imshow(X_test_orig[i], interpolation='nearest')
+    plt.text(0, 0, predicted[i], color='black', 
+             bbox=dict(facecolor='white', alpha=1))
+    plt.axis('off')
+    
+# Function for constructing the convolution neural network
+# Feel free to add parameters, if you want
+
+def build_model(num_conv = 1, conv_activation = "relu", num_dense = 1, dense_activation  = "relu", 
+               dropout = True, max_pooling = True):
+    """"""
+    model = Sequential()
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv), 
+                     padding='valid',
+                     input_shape=shape_ord))
+    model.add(Activation(conv_activation))
+    
+    for i in range(num_conv-1):
+        model.add(Conv2D(nb_filters, (nb_conv, nb_conv)))
+        model.add(Activation(conv_activation))
+        
+    if max_pooling is True:
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+        
+    if dropout is True:
+        model.add(Dropout(0.25))
+    
+    model.add(Flatten())
+
+    if dropout is True:
+        model.add(Dropout(0.5))
+    
+    for i in range(num_dense-1):
+        model.add(Dense(128))
+        model.add(Activation(dense_activation))
+        
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    
+    model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+    model.fit(X_train, Y_train, batch_size=batch_size, 
+              epochs=nb_epoch,verbose=1,
+              validation_data=(X_test, Y_test))
+          
+
+    #Evaluating the model on the test data    
+    score, accuracy = model.evaluate(X_test, Y_test, verbose=0)
+    print(num_conv , 'convolutional layers,', num_dense, "dense layers")
+    if max_pooling: print("With max pooling")
+    if dropout: print("With dropout")
+    print('Test score:', score)
+    print('Test accuracy:', accuracy)
+    slice = 15
+    predicted = model.predict(X_test[:slice]).argmax(-1)
+
+    plt.figure(figsize=(16,8))
+    for i in range(slice):
+        plt.subplot(1, slice, i+1)
+        plt.imshow(X_test_orig[i], interpolation='nearest')
+        plt.text(0, 0, predicted[i], color='black', 
+             bbox=dict(facecolor='white', alpha=1))
+        plt.axis('off')
+ 
+ build_model(num_conv = 4, num_dense = 4)
+
+#Train on 10 different numbers
+
+#Load the training and testing data
+#x_train and x_test parts contain greyscale RGB codes (from 0 to 255) while y_train and y_test parts contains labels from 0 to 9 which represents which number they actually are. 
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+from keras import backend as K
+
+img_rows, img_cols = 28, 28
+
+if K.image_data_format() == 'channels_first':
+    shape_ord = (1, img_rows, img_cols)
+else:  # channel_last
+    shape_ord = (img_rows, img_cols, 1)
+
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+build_model(num_conv = 3, num_dense = 3)
+
